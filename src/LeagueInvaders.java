c333652f3a8f1c5407d1f83b50bcc13d23c96c94
ethan <ethan@LAPTOP-FHOLCKVQ.lan>
2021-04-25 14:26:55 -0700

    25/04 League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,32 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	
+	//initial variables
+	JFrame primaryframe;
+	static final int WIDTH = 500;
+	static final int HEIGHT = 800;
+	
+	
+	// main method
+	public static void main(String[] args) {
+		LeagueInvaders LI = new LeagueInvaders();
+		LI.initialize();
+		LI.setup();
+	}
 
+	
+	// initialize primaryframe
+	public void initialize() {
+		primaryframe = new JFrame("League Invaders");
+	}
+	
+	
+	// set up primaryframe
+	public void setup() {
+		primaryframe.setSize(WIDTH,HEIGHT);
+		primaryframe.setVisible(true);
+		primaryframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
